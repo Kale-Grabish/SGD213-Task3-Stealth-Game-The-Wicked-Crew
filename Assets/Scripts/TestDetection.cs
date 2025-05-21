517f1077d8883044e3dd51419c0f9f64ecd91c56
uscxav <a_a246@student.usc.edu.au>
2025-05-21 11:24:36 +1000

    IDetection
    - 5 second timer

--- Assets/Scripts/TestDetection.cs
+++ Assets/Scripts/TestDetection.cs
@@ -2,18 +2,42 @@ using UnityEngine;
 
 public class TestDetection : MonoBehaviour, IDetection
 {
+    public float timer = 0f;
+    public float maxTime = 5f;
+
     public void Alert()
     {
+        if (timer <= maxTime)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= maxTime)
+            {
+                Debug.Log("Max time reached");
+                // destroy player
+
+            }
+        }
 
     }
 
     public void Calm()
     {
-        
+        timer = 0f;
+        Debug.Log("timer reset");
     }
 
     public void OnTriggerEnter(Collider other)
     {
         Debug.Log("collision detected");
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        Alert();
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        Calm();
     }
 }
