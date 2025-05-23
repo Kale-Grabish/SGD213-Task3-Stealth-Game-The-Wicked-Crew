7cc304f9af1f5b265ec94259fb55eff0c86cfb39
NedTanner <ert015@student.usc.edu.au>
2025-05-23 19:13:04 +1000

    Player Update!
    - Added the player
    - Player can now be played
    - Playable character added
    - Fixed bug with player not having a playable character
    - Deleted haroldbrine
    - Added textmeshpro for text stuff

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement playerMovement;
+
+    // Allows other scripts access these variables without cluttering the inspector with public variable stuff :)
+    public Vector2 MoveInput { get; private set; }
+    public Vector2 LookInput { get; private set; }
+    public float crouchCooldown = 0.3f;
+
+    private float lastToggleTime = -Mathf.Infinity;
+    private Animator playerAnimator;
+
+    private void Awake()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        MoveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        LookInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        // Cooldown helps prevent buggy animations and general jank
+        if (Input.GetKey(KeyCode.LeftControl) && Time.time >= lastToggleTime + crouchCooldown)
+        {
+            playerMovement.Crouch();
+            lastToggleTime = Time.time;
+        }
+
+        LeanCheck();
+    }
+
+    void LeanCheck()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            playerAnimator.SetBool("IsLeaningLeft", true);
+            playerAnimator.SetBool("IsLeaningRight", false);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            playerAnimator.SetBool("IsLeaningRight", true);
+            playerAnimator.SetBool("IsLeaningLeft", false);
+        }
+        else
+        {
+            playerAnimator.SetBool("IsLeaningLeft", false);
+            playerAnimator.SetBool("IsLeaningRight", false);
+        }
+    }
+}
+    
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private PlayerInput playerInput;
+    private bool isCrouched = true;
+    private float xRotation = 0f;
+    private Camera playerCamera;
+    private Animator playerAnimator;
+
+    public float walkSpeed = 10f;
+    public float crouchSpeed = 7f;
+    public float mouseSensitivity = 100f;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+        playerAnimator = GetComponent<Animator>();
+        playerCamera = GetComponentInChildren<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        LookCheck();
+    }
+
+    void FixedUpdate()
+    {
+        // Obtains movement direction defined in 'PlayerInput.cs'
+        Vector3 movement = new Vector3(playerInput.MoveInput.x, 0f, playerInput.MoveInput.y);
+
+
+        // Checks crouch status, defines movement speed
+        if (isCrouched == true)
+        {
+            movement = transform.TransformDirection(movement.normalized) * crouchSpeed;
+            movement.y = rb.linearVelocity.y; // Maintains gravity instead of overriding it
+        }
+
+        else
+        {
+            movement = transform.TransformDirection(movement.normalized) * walkSpeed;
+            movement.y = rb.linearVelocity.y; // Maintains gravity instead of overriding it
+        }
+        // Applies movement directly to player Rigidbody
+        rb.linearVelocity = movement;
+
+    }
+
+    void LookCheck()
+    {
+        Vector2 look = playerInput.LookInput * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= look.y;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.Rotate(Vector3.up * look.x); // Rotates the whole body on the Y axis
+        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // Only apply pitch rotation to camera
+    }
+
+    public void Crouch()
+    {
+        // Toggle animation parameter to opposite bool
+        bool current = playerAnimator.GetBool("IsCrouched");
+        playerAnimator.SetBool("IsCrouched", !current);
+    }
+}
